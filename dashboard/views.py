ce0f7db9605eeaaa1fd0f1ae64f5acaa47ee82b0
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-02-14 00:57:30 -0500

    Added ajax call to save modules positions

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+import json
 
-def home(request):
-
-    # Specify what modules to include in the dashboard
-    # TODO: add to user settings
-    modules = [
-        {
+# Specify what modules to include in the dashboard
+# TODO: add to user settings
+modules = {
+        'dt': {
             'id': 'dt',
             'styles': 'dt/includes/dt_styles.html',
             'scripts': 'dt/includes/dt_scripts.html',
@@ -13,7 +13,7 @@ def home(request):
             'top': 200,
             'left': 10
         },
-        {
+        'weather': {
             'id': 'weather',
             'styles': 'weather/includes/weather_styles.html',
             'scripts': 'weather/includes/weather_scripts.html',
@@ -21,38 +21,35 @@ def home(request):
             'top': 50,
             'left': 100
         }
-    ]
+}
 
+def home(request):
     context = {
         'modules': modules
     }
     return render(request, 'dashboard/dashboard.html', context)
 
 def update(request):
-    # TODO: add to user settings
-    modules = [
-        {
-            'id': 'dt',
-            'styles': 'dt/includes/dt_styles.html',
-            'scripts': 'dt/includes/dt_scripts.html',
-            'page': 'dt/dt.html',
-            'top': 200,
-            'left': 10
-        },
-        {
-            'id': 'weather',
-            'styles': 'weather/includes/weather_styles.html',
-            'scripts': 'weather/includes/weather_scripts.html',
-            'page': 'weather/weather.html',
-            'top': 50,
-            'left': 100
-        }
-    ]
-
     context = {
         'modules': modules
     }
     return render(request, 'dashboard/dashboard_update.html', context)
 
 def save_update(request):
-    pass
+    if request.method == 'GET':
+        id_data = request.GET.getlist('id_data[]')
+
+        """
+        for data_json in id_data:
+            data = json.loads(data_json)
+            i = data['id']
+            modules[i]['top'] = int(data['top'])
+            modules[i]['left'] = int(data['left'])
+        print(f'New modules:\n{modules}')
+        """
+        context = {
+            'blah': 'blah'
+        }
+        return JsonResponse(context)
+
+    # TODO no valid return if method isn't GET
