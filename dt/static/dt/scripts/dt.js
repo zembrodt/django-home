40c03e1deadd33219458313b59b1df4bd15cc42a
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2019-03-06 15:37:16 -0500

    Fixed includes for base django home js file

--- dt/static/dt/scripts/dt.js
+++ dt/static/dt/scripts/dt.js
@@ -1,22 +1,5 @@
 /* Datetime module scripts */
 
-// Function to pad leading zeros
-Number.prototype.pad = function(size) {
-    var s = String(this);
-    while (s.length < (size || 2)) {s = "0" + s;}
-    return s;
-}
-
-const seconds_inc = 1000;
-const minutes_inc = seconds_inc * 60;
-const hours_inc = minutes_inc * 60;
-const month_names = [
-    "January", "February", "March",
-    "April", "May", "June",
-    "July", "August", "September",
-    "October", "November", "December"
-];
-
 class Datetime {
     constructor(id) { // other things?
         this.id = id;
